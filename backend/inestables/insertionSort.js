7ddfd547ad0eae9096cb6c8b9f41c2569a8ec202
Consgomez <a01026717@itesm.mx>
2020-05-19 17:09:31 -0500

    Counting

--- backend/inestables/insertionSort.js
+++ backend/inestables/insertionSort.js
@@ -1,8 +1,8 @@
 function insertionSort(a)
 {
-    for (var i = 1; i < a.length; i++) {
-      var j = i - 1;
-      var tmp = a[i];
+    for (let i = 1; i < a.length; i++) {
+      let j = i - 1;
+      let tmp = a[i];
       while (j >= 0 && a[j] > tmp) {
         a[j + 1] = a[j]
         j--
