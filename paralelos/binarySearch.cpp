7a64994403fe9053674a9d10d6fb062705c5dbee
monterrubio-miguel <monterrubio.miguel@gmail.com>
2020-06-02 23:00:27 -0500

    test

--- paralelos/binarySearch.cpp
+++ paralelos/binarySearch.cpp
@@ -7,7 +7,7 @@ using namespace std;
 // A recursive binary search function. It returns 
 // location of x in given array arr[l..r] is present, 
 // otherwise -1 
-int binarySearch(int arr[], int l, int r, int x) 
+int binarySearch(vector<int> arr, int l, int r, int x) 
 { 
     if (r >= l) { 
         int mid = l + (r - l) / 2; 
@@ -34,30 +34,148 @@ int binarySearch(int arr[], int l, int r, int x)
   
 int main(void) 
 { 
-    int arr[] = { 2, 3, 4, 10, 40 }; 
+    int arr[] = { 2, 3, 4, 10, 40 , 41, 42, 43}; 
     int x = 10; 
     int n = sizeof(arr) / sizeof(arr[0]); 
-    int result = binarySearch(arr, 0, n - 1, x); 
 
-    int maxThreads = omp_get_max_threads();
+    // int maxThreads = omp_get_max_threads();
+    int maxThreads = 4;
 
     omp_set_num_threads(maxThreads);
 
+    //checks that there are not more threads than values in the array
     while(maxThreads > n)
     {
         maxThreads -= 1;
     }
 
-    int subSize = n/maxThreads;
-    int subArrs[maxThreads][subSize];
-
-    // cout << maxThreads << endl;
-    for (int i = 0; i < n; i++)
+    //this checks that each subarray is of equal length
+    while(n%maxThreads > 0)
     {
-        for (int j = 0; j < subSize; i++)
+        maxThreads -= 1;
+    }
+
+    int subSize = n/maxThreads;
+    vector<int> subArrays[maxThreads];
+
+    //assigns numbers to each subarray
+    for (int i = 0; i < subSize; ++i)
+    {
+        for (int j = 0; j < maxThreads; ++j)
         {
-            subArrs[i][j] = 
+            subArrays[j].push_back(arr[j*subSize+i]);
         }
+    }
+
+    cout << endl;
+
+    int result, result1, result2, result3, result4;
+
+    //for debugging
+    // for (int i = 0; i < maxThreads; i++) { 
+  
+    //     cout << "Elements at index "
+    //          << i << ": "; 
+    //     for (auto it = subArrays[i].begin(); 
+    //          it != subArrays[i].end(); it++) { 
+    //         cout << *it << ' '; 
+    //     } 
+    //     cout << endl; 
+    // } 
+
+    switch(maxThreads)
+    {
+        case 2:
+            #pragma omp parallel sections num_threads(maxThreads)
+            {
+                #pragma omp section
+                {
+                    result1 = binarySearch(subArrays[0], 0, n - 1, x); 
+                }
+                #pragma omp section
+                {
+                    result2 = binarySearch(subArrays[1], 0, n - 1, x); 
+                }
+            }
+            if (result1 != -1)
+            {
+                result = result1;
+            }
+            else if (result2 != -2)
+            {
+                result = result2;
+            }
+            break;
+        case 3:
+            #pragma omp parallel sections num_threads(maxThreads)
+            {
+                #pragma omp section
+                {
+                    result1 = binarySearch(subArrays[0], 0, n - 1, x); 
+                }
+                #pragma omp section
+                {
+                    result2 = binarySearch(subArrays[1], 0, n - 1, x); 
+                }
+                #pragma omp section
+                {
+                    result3 = binarySearch(subArrays[2], 0, n - 1, x); 
+                }
+            }
+            if (result1 != -1)
+            {
+                result = result1;
+            }
+            else if (result2 != -1)
+            {
+                result = result2;
+            }
+            else if (result3 != -1)
+            {
+                result = result3;
+            }
+            break;
+        case 4:
+            #pragma omp parallel sections num_threads(maxThreads)
+            {
+                #pragma omp section
+                {
+                    result1 = binarySearch(subArrays[0], 0, n - 1, x); 
+                }
+                #pragma omp section
+                {
+                    result2 = binarySearch(subArrays[1], 0, n - 1, x); 
+                }
+                #pragma omp section
+                {
+                    result3 = binarySearch(subArrays[2], 0, n - 1, x); 
+                }
+                #pragma omp section
+                {
+                    result4 = binarySearch(subArrays[3], 0, n - 1, x); 
+                }
+            }
+            if (result1 != -1)
+            {
+                result = result1;
+            }
+            else if (result2 != -1)
+            {
+                result = result2;
+            }
+            else if (result3 != -1)
+            {
+                result = result3;
+            }
+            else if (result4 != -1)
+            {
+                result = result4;
+            }
+            break;
+        default:
+            int result = binarySearch(subArrays[0], 0, n - 1, x); 
+
+
     }
 
     if (result == -1)
