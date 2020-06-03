ced3d475dccf64b3552ef7c44f8ee221c43e22a3
danielroa98 <daniel.roa98@gmail.com>
2020-06-02 22:40:48 -0500

    Versión sin terminar

---
+++ paralelos/heapSortPrueba.cpp
@@ -0,0 +1,95 @@
+#include <bits/stdc++.h>
+#include <iostream>
+#include <omp.h>
+
+using namespace std;
+
+//Se remueve el primer elemento del arreglo
+void siftDown( int *a, int k, int N); 
+
+void swap(int *m, int *n)
+{
+	int tmp;
+	tmp = *m;
+	*m = *n;
+	*n = tmp;
+}
+
+//N = array size
+void heapsort( int a[], int N){       
+	/* heapify */    
+	for (int k = N/2; k >= 0; k--) { 
+		siftDown( a, k, N);    
+	}     
+
+	while (N-1 > 0) {  
+		/* swap the root(maximum value) of the heap 
+		with the last element of the heap */
+        #pragma omp parallel sections num_threads(8)
+        {
+            #pragma omp section
+            {
+            swap(a[N-1], a[0]); 
+            }
+            /* put the heap back in max-heap order */
+            #pragma omp section
+            {
+            siftDown(a, 0, N-1);  
+            /* N-- : decrease the size of the heap by one 
+            so that the previous max value will
+                stay in its proper placement */
+            N--;
+            }
+        }
+	}
+}  
+
+void siftDown( int *a, int k, int N){     
+	while ( k*2 + 1 < N ) { 
+		/* For zero-based arrays, the children are 2*i+1 and 2*i+2 */
+ 		int child = 2*k + 1;    
+
+		/* get bigger child if there are two children */
+		if ((child + 1 < N) && (a[child] < a[child+1])) child++;        
+       
+		if (a[k] < a[child]) {   /* out of max-heap order */        
+			swap( a[child], a[k] );  
+			/* repeat to continue sifting down the child now */         
+			k = child;  
+		}        
+		else            
+			return;   
+	}
+}   
+
+int main()
+{    
+	int i;    
+    int no = 15;
+    int a[no];
+    int nrand =no*10;
+
+    srand((unsigned)time(0));
+
+	for(int i = 0; i < no; i++){
+	    a[i] = (rand()%nrand)+1;
+	}
+
+	const size_t sz = sizeof(a)/sizeof(a[0]); 
+
+    printf("\nAntes de ordenar:\n");
+
+	for (i = 0; i < sz; i++) 
+		cout << a[i] << " ";
+	cout << "\n";
+
+	heapsort(a, sz);     
+
+    cout << "\nDespués de ordenar:\n";
+
+	for (i = 0; i < sz; i++) 
+		cout << a[i] << " ";
+	cout << endl;
+
+	return 0;
+}
