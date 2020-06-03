e90eab4a641c49921ad59f664173f6d7eb995d54
danielroa98 <daniel.roa98@gmail.com>
2020-06-02 19:39:16 -0500

    Comentarios

--- paralelos/mergeSort.cpp
+++ paralelos/mergeSort.cpp
@@ -1,22 +1,18 @@
-//merge sort parallel
-//compile with -fopenmp flag 
-//to calculate time taken on linux, use "time" keyword while executing object file
-
 #include<iostream>
-#include "omp.h"
+#include <omp.h>
 using namespace std;
-//performs the bottom up combine operation
+
 void merge(int a[], int l, int m, int r)
 {
 	int temp[m-l+1], temp2[r-m];
-	//copy first part to temporary array
+
 	for(int i=0; i<(m-l+1); i++)
 		temp[i]=a[l+i];
-	//copy second part to temporary arrat]y
+
 	for(int i=0; i<(r-m); i++)
 		temp2[i]=a[m+1+i];
 	int i=0, j=0, k=l;
-	//combine the arrays on the basis of order
+
 	while(i<(m-l+1) && j<(r-m))
 	{
 		if(temp[i]<temp2[j])
@@ -24,40 +20,37 @@ void merge(int a[], int l, int m, int r)
 		else
 			a[k++]=temp2[j++];
 	}
-	//to combine the remainder of the two arrays
+
 	while(i<(m-l+1))
 		a[k++]=temp[i++];
 	while(j<(r-m))
 		a[k++]=temp2[j++];
 
 }
-/* 
-This function is the main hotspot of the algorithm which I have chosen to parallelise
-The calls labelled call 1 and call 2 are essentially independent since they are operating on distinct
-parts of the array and hence can be called in parallel.
-*/
 
 void mergeSort(int a[], int l, int r)
 {
 	if(l<r)
 	{
-		//cout<<"came here";
 		int m=(l+r)/2;
+
+		//Se paraleliza esta sección debido a que estas dos instancias pueden correr a la par
+		//esto es debido a que uno maneja el arreglo original y el otro maneja el arreglo
+		//que se está ordenando
 		#pragma omp parallel sections num_threads(2)
-		{
+		{	
 			#pragma omp section
 			{
-				mergeSort(a,l,m); //call 1
+				mergeSort(a,l,m); 
 			}
 			#pragma omp section
 			{
-				mergeSort(a,m+1,r); //call 2
+				mergeSort(a,m+1,r);
 			}
 		}
-		merge(a,l,m,r); //this function is outside the parallel sections since it merges and access the same data 
+		merge(a,l,m,r);
 	}
 }
-//a simple print function to print all the elements
 void print(int a[], int n)
 {
 	for(int i=0; i<n; i++)
@@ -66,21 +59,23 @@ void print(int a[], int n)
 }
 int main()
 {
-	 int n = 100;
+	int n = 100;
     int a[n];
     int nrand =n*10;
 
     srand((unsigned)time(0));
 
+	//Generación aleatoria de datos
 	for(int i = 0; i < n; i++){
 	    a[i] = (rand()%nrand)+1;
 	}
 
-	cout<<"\nUnsorted array; ";
+	//Impresión del arreglo sin ordenar
+	cout<<"\nArreglo sin ordenar; \n";
 	print(a,n);
-	//call should be to a wrapper function that calls the mergeSort function
+
 	mergeSort(a,0,n-1);
-	cout<<"\nSorted array: ";
+	cout<<"\nArreglo ordenado: \n";
 	print(a,n);
 	return 0;
 }
